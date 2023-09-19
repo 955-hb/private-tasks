c76ba23c105bd4f78a2a0321d52cf7fc93d317f6
Hannes Bröhl <hannes.broehl@icloud.com>
2023-09-19 10:40:23 +0200

    fetch()-tutorial

---
+++ fetch()-tutorial/app.js
@@ -0,0 +1,23 @@
+
+
+fetch('data.json').then((Response) => {
+    return Response.json();
+})
+.then((users) => {
+    const container = document.getElementById('users');
+
+
+    users.forEach((user) => {
+        const tmpl = document.getElementById('user-card-template').content.cloneNode(true);
+
+       tmpl.querySelector('.fullname').innerText = user.fullname;
+
+       tmpl.querySelector('.profession').innerText = user.profession;
+
+       tmpl.querySelector('.description').innerText = user.description;
+
+       tmpl.querySelector('img').setAttribute('src', user.avatar);
+
+       container.appendChild(tmpl);
+    });
+})
---
+++ fetch()-tutorial/readme.md
@@ -0,0 +1,4 @@
+######
+
+youtube-Link zum tutorial:
+https://www.youtube.com/watch?v=jv_cs0I60C4
