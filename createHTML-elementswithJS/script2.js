c23d7bba4c0ba84b1ade191bd51f16a3d07ecdc2
Hannes Bröhl <hannes.broehl@icloud.com>
2023-09-26 10:19:29 +0200

    create HTML-element

---
+++ createHTML-elementswithJS/script2.js
@@ -0,0 +1,13 @@
+console.log('Hallo');
+
+
+// neue H1 erzeugen
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+
+    new div = document.createElement('div');
+    const divText = document.createTextNode('vier');
+    newDiv.appendChild(divText);
+    
+})
