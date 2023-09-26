d6845dec957172bab298851a1e8ff84f4470d9a7
Hannes Bröhl <hannes.broehl@icloud.com>
2023-09-26 10:19:46 +0200

    JSON

--- JSON-tutorial/script.js
+++ JSON-tutorial/script.js
@@ -1,20 +1,4 @@
-let users = `[
-    {
-        "name": "Max",
-        "sureName": "Mustermann",
-        "age": 33, 
-        "birthyear": 1990,
-        "online": true,
-        "hobbys": ["biking", "swimming", "relaxing"]
-    },
-    {
-        "name": "Marion",
-        "sureName": "Musterfrau",
-        "age": 37, 
-        "birthyear": 1984,
-        "online": false,
-        "hobbys": ["relaxing", "coding", "drawing"]
-    }
-]`;
+console.log('Hello World!');
 
-console.log(JSON.parse(users));
+
+
