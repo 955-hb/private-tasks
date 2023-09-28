cbb355db1ac61b3a2fe3419fbcc54183d9f8bbb7
Hannes Bröhl <hannes.broehl@icloud.com>
2023-09-28 09:51:24 +0200

    update JSON import

--- JSON-tutorial/script.js
+++ JSON-tutorial/script.js
@@ -1,4 +1,31 @@
-console.log('Hello World!');
+console.log('Hello JSON!');
+
+
+// JSON-PARSE()
+const meJson = `
+      {
+          "name": "Hannes",
+          "surname": "Mustermann",
+          "age": 23,
+          "favoriteColors": [
+              "blue",
+              "green"
+          ]
+      }`;
+
+const user = JSON.parse(meJson);
+console.log(user);
+console.log(user.age)
 
 
 
+// ################################################
+console.log("################################################");
+// JSON-STRINGIFY()
+const car = {
+    marke: "Honda",
+    model: "Civic"
+};
+
+const carJson = JSON.stringify(car);
+console.log(carJson)
