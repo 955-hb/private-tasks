04efa001b9da41a11d1d4fea34e13dcacc0bea7b
Hannes Bröhl <hannes.broehl@icloud.com>
2023-10-03 12:59:27 +0200

    update change -> commits

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,22 +1,24 @@
 // Hallo Hannes
 
 function randomPassword () {
-    let displayPassword = document.getElementById('display-password');
-    let passwordLength = document.getElementById('password-length').value;
-    let error = document.getElementById('error-info');
+    let displayPassword = document.getElementById('display-password'); //const
+    let passwordLength = document.getElementById('password-length').value; //const
+    let error = document.getElementById('error-info'); //const
     let password = '';
-    let characters = 
+    let characters = //const
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890<^<^1234567890ß#+*.,;:[]|{}/()=?!"§$%&';
 
     //if user not selected length
-    parseInt(passwordLength);
-    if (passwordLength == 0) {
+    console.log(typeof passwordLength);
+    if (parseInt(passwordLength) === 0) {
         error.innerHTML = 'Please select password length';
-        error.style.padding = '10px 15px';
+        error.style.padding = '10px 15px'; // direkt css
+
+        //nicht mit timeout arbeiten. --> event
         setTimeout(() => {
             error.style.visibility = 'hidden';
             window.location.reload()
-        }, 2000);
+        }, 2000); // länger
     }
 
 
